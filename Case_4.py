8640dd885d1b20dc2f8e110dcc9b41b6148eba36
RamezControll <r.karius@student.tudelft.nl>
2026-03-02 15:50:59 +0100

    Run data from another local map

--- Case_4.py
+++ Case_4.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 from read_telemetry_ecg import read_ecg_mat
 
-ecg, fs, t = read_ecg_mat("005_Pimpel.mat", plotresult=True)
+ecg, fs, t = read_ecg_mat("../Data_E2/005_Pimpel.mat", plotresult=True)
 
 fig = plt.figure(figsize=(9, 12))
 plt.plot(t, ecg)
